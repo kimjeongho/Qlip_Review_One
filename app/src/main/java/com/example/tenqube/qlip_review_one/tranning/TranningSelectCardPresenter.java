32c2e3c7bb231a032c627bd9ce59f3010bebe732
kimjeongho <jeongho9496@daum.net>
2016-06-07 18:14:17 +0900

    ReviewOne 수정

--- app/src/main/java/com/example/tenqube/qlip_review_one/tranning/TranningSelectCardPresenter.java
+++ app/src/main/java/com/example/tenqube/qlip_review_one/tranning/TranningSelectCardPresenter.java
@@ -1,7 +1,19 @@
 package com.example.tenqube.qlip_review_one.tranning;
 
+import android.widget.Toolbar;
+
+import com.example.tenqube.qlip_review_one.data.CardTableData;
+
+import java.util.ArrayList;
+
 /**
  * Created by tenqube on 2016-06-07.
  */
 public interface TranningSelectCardPresenter {
+    void settingToolbar(Toolbar toolbar);
+    ArrayList<CardTableData> loadCardData();
+    void confirm(ArrayList<CardTableData> selectedCardList);
+    void deleteConfirm(ArrayList<CardTableData> cardList, String cardName);
+    void updateTextConfirm(ArrayList<CardTableData> cardList, String changeCardName, int cardType);
+    void onClickCardType(int id);
 }
